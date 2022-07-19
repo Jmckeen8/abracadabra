06902e9b3feddf478f8fef82e0b8e7acefcfae9b
FrancArsen <sfang3@wpi.edu>
2022-07-18 23:51:00 -0400

    Update MyScript.cs

--- Assets/Speech Recognition/MyScript.cs
+++ Assets/Speech Recognition/MyScript.cs
@@ -1,9 +1,13 @@
 using UnityEditor.Scripting.Python;
 using UnityEditor;
 using UnityEngine;
+using System.Threading;
+
 
 public class MyScript : MonoBehaviour
 {
+    Thread srThread;
+    
     void Start()
     {
         srThread = new Thread(runScript);
